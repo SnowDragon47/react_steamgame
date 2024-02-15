4905e771e81b89cc0620ce61bbf508d9122b34ee
AshleyTorray <hogarden1135j@gmail.com>
2024-02-14 22:55:11 -0800

    upgrade login & RWD design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './style.min.css';
 import './libs.min.css';
+import React, {useState} from "react";
+
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import Home from './pages/Home.jsx';
 import Howtostart from './pages/Howtostart.jsx';
@@ -7,9 +9,16 @@ import Support from './pages/Support.jsx';
 import Footer from "./pages/Layout/Footer.jsx";
 import Header from './pages/Layout/Header.jsx';
 import Rules from './pages/Rules.jsx';
-import Login from './pages/User/Login.jsx';
+import Logined from './pages/User/Logined.jsx';
+import NotLogined from './pages/User/NotLogined.jsx';
 
 function App() {
+  const [islogin, setLogin] = useState(false);
+
+  const toogleLogin = () => {
+          setLogin(!islogin)
+  }
+
   return (
       <body className='m-page'>
         <div className='wrapper'>
@@ -20,7 +29,8 @@ function App() {
               <Route path='/howtostart' element = {<Howtostart/>} />
               <Route path='/support' element = {<Support/>} />
               <Route path='/rules' element = {<Rules/>} />
-              <Route path = '/user/login' element = {<Login/>}/>
+              <Route path = '/user/login' element = {<Logined/>}/>
+              <Route path='/user/logined' element = {<NotLogined/>}/>
             </Routes>
             <Footer/>
           </BrowserRouter>      
--- src/pages/User/Logined.jsx
+++ src/pages/User/Logined.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Table, Col, Row, Container} from "react-bootstrap";
+import LoginImg from '../../assets/img/login.jpg';
+import Loginbar1 from '../../assets/img/loginbar1.svg';
+import Loginbar3 from '../../assets/img/Group 3.svg';
+
 import LoginInfoCard from "../../assets/img/Info Card.png";
 import ArtistAvatar from "../../assets/img/Artist Avatar.png";
 import ArtistAvatar1 from "../../assets/img/Artist Avatar1.png";
@@ -12,144 +15,157 @@ const Logined = () =>
 {
     return (
         <div>
-            <div className="login_container">
-                <div className="artist_card">
-                    <tbody>
-                        <tr>
-                            <td>
-                                <img src={LoginInfoCard} className="login_info_card"/>
-                            </td>
-                            <td>
-                                <tr className="back_content_number">
-                                    <td>
-                                        <img src={ArtistAvatar} width={40}/>
-                                    </td>
-                                    <td>
-                                        <tr>
-                                            <text className="font_work_sans ml-2">
-                                                Wallet
-                                            </text>
-                                        </tr>
-                                        <tr>
-                                            <button className="wallet_number">
-                                                0X....4888
-                                             </button>   
-                                        </tr>
-                                        
-                                    </td>
-                                </tr>
-                                <tr className="back_content_claim">
-                                    <td>
-                                        <tr>
-                                            <text className="font_work_sans">
-                                                Available To Claim
-                                            </text>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <text className="text10" style={{marginLeft:"-80%"}}>
-                                                    Total:
-                                                </text>
-                                            </td>
-                                            <td>
-                                                <text className="text14" style={{marginLeft:"-90%"}}>
-                                                    1203 SCRP
-                                                </text>
-                                            </td>
-                                        </tr>
-                                    </td>
-                                </tr>
-                                <tr className="wallet_btn_red_claim">
-                                    <td>
-                                        <text className="font_work_sans ml-2">
-                                            CLAIM
-                                        </text>
-                                    </td>
-                                </tr>
-                                
-                            </td>
-                            <td>
-                                <tr className="back_content_address">
-                                    <td>
-                                        <img src={ArtistAvatar1} width={40}/>
-                                    </td>
-                                    <td>
-                                        <tr>
-                                            <text className="font_work_sans ml-2">
-                                                Wallet Address
-                                            </text>
-                                        </tr>
-                                        <tr>
-                                            <button className="wallet_address">
-                                            75651198042333132
-                                            </button>
-                                        </tr>
-                                        
-                                    </td>
-                                </tr>
-                                <tr className="back_content_tier">
-                                    <td>
-                                        <tr>
-                                            <text className="font_work_sans">
-                                                Current Tier
-                                            </text>
-                                        </tr>
-                                        <tr>
-                                            <td>
-                                                <text className="text10" style={{marginLeft:"-50%"}}>
-                                                    Pool access:
-                                                </text>
-                                            </td>
-                                            <td>
-                                                <text className="text14">
-                                                    Tier 1
-                                                </text>
-                                            </td>
-                                        </tr>
-                                    </td>
-                                </tr>
-                                
-                                <tr className="wallet_btn_red_upgrade">
-                                    <td>
-                                        <text className="font_work_sans ml-2">
-                                            UPGRADE
-                                        </text>
-                                    </td>
-                                </tr>
-                            </td>
-                        </tr>
-                    </tbody>
+            <main className="content">  
+                <div class="main main2">
+                    <div class="main__video">
+                        <img src={LoginImg} alt=""/>
+                    </div>
                 </div>
-            </div>
-            <div className="logined_user_list">
-                <img src={LoginBetweenImg} className="login_between_bar"/>
-                <table className="user_table">
-                    <thead className="table_header">
-                        <td>
-                            <text className="ml-4 rank">#</text>
-                        </td>                  
-                        <td>    
-                        <text className="ml-4 user_avatar">Player</text>
-                        </td>
-                        <td>
-                        <text className="ml-20 tier">
-                                Tier
-                            </text>
-                        </td>
-                        <td>
-                        <text className="ml-8 server">
-                                Server
-                            </text>
-                        </td>
-                        <td>
-                        <text className="ml-8 point">
-                                Points earned
-                            </text>
-                        </td>                            
-                    </thead>
-                    <tbody className="table_body">
+            </main> 
+            <div className="login">  
+
+                <img src={Loginbar1} alt="" className="login_bar1"/>
+                <div className="login_container">
+
+                    <div className="artist_card">
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <img src={LoginInfoCard} className="login_info_card"/>
+                                </td>
+                                <td>
+                                    <tr className="back_content_number">
+                                        <td>
+                                            <img src={ArtistAvatar} width={40}/>
+                                        </td>
+                                        <td>
+                                            <tr>
+                                                <text className="font_work_sans ml-2">
+                                                    Wallet
+                                                </text>
+                                            </tr>
+                                            <tr>
+                                                <button className="wallet_number">
+                                                    0X....4888
+                                                </button>   
+                                            </tr>
+                                            
+                                        </td>
+                                    </tr>
+                                    <tr className="back_content_claim">
+                                        <td>
+                                            <tr>
+                                                <text className="font_work_sans">
+                                                    Available To Claim
+                                                </text>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <text className="text10" style={{marginLeft:"-80%"}}>
+                                                        Total:
+                                                    </text>
+                                                </td>
+                                                <td>
+                                                    <text className="text14" style={{marginLeft:"-90%"}}>
+                                                        1203 SCRP
+                                                    </text>
+                                                </td>
+                                            </tr>
+                                        </td>
+                                    </tr>
+                                    <tr className="wallet_btn_red_claim">
+                                        <td>
+                                            <text className="font_work_sans ml-2">
+                                                CLAIM
+                                            </text>
+                                        </td>
+                                    </tr>
+                                    
+                                </td>
+                                <td>
+                                    <tr className="back_content_address">
+                                        <td>
+                                            <img src={ArtistAvatar1} width={40}/>
+                                        </td>
+                                        <td>
+                                            <tr>
+                                                <text className="font_work_sans ml-2">
+                                                    Wallet Address
+                                                </text>
+                                            </tr>
+                                            <tr>
+                                                <button className="wallet_address">
+                                                75651198042333132
+                                                </button>
+                                            </tr>
+                                            
+                                        </td>
+                                    </tr>
+                                    <tr className="back_content_tier">
+                                        <td>
+                                            <tr>
+                                                <text className="font_work_sans">
+                                                    Current Tier
+                                                </text>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <text className="text10" style={{marginLeft:"-50%"}}>
+                                                        Pool access:
+                                                    </text>
+                                                </td>
+                                                <td>
+                                                    <text className="text14">
+                                                        Tier 1
+                                                    </text>
+                                                </td>
+                                            </tr>
+                                        </td>
+                                    </tr>
+                                    
+                                    <tr className="wallet_btn_red_upgrade">
+                                        <td>
+                                            <text className="font_work_sans ml-2">
+                                                UPGRADE
+                                            </text>
+                                        </td>
+                                    </tr>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </div>
+                </div>
+                <div className="logined_user_list">
+                    <img src={LoginBetweenImg} className="login_between_bar"/>
+                    <table className="user_table">
+                        <thead className="table_header">
+                            <tr style={{width:"100%"}}>
+                            <td>
+                                <text className="rank">#</text>
+                            </td>                  
+                            <td>    
+                            <text className="ml-4 user_avatar">Player</text>
+                            </td>
+                            <td>
+                            <text className="ml-20 tier">
+                                    Tier
+                                </text>
+                            </td>
+                            <td>
+                            <text className="ml-8 server">
+                                    Server
+                                </text>
+                            </td>
+                            <td>
+                            <text className="ml-8 point">
+                                    Points earned
+                                </text>
+                            </td>
+                            </tr>             
+                        </thead>
+                        <tbody className="table_body">
                             <tr className="user_member_btn">
-                                <td className="text18 ml-4 rank">
+                                <td className="ml-4 rank text18">
                                         <img src={RankingNumberImg}/>
                                         <text className="ranking_number">1</text>
                                 </td>
@@ -212,9 +228,11 @@ const Logined = () =>
                                     <text className="userInfo point">15120</text>
                                 </td>
                             </tr>
-                        
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                    <img src={Loginbar3} alt="" className="login_bar3"/>
+                </div>
+
             </div>
         </div> 
  
